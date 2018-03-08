8ed9b5d1c0058e7aac4490d1de6f939bfb2d09ab
Spencer Edgecombe <se185066@wcajc185196-02y.corp.ncr.com>
2018-03-08 13:30:22 -0500

    Add watch version

---
+++ journi wear Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  journi wear Extension
+//
+//  Created by Spencer Edgecombe on 3/8/18.
+//  Copyright © 2018 fwt. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
